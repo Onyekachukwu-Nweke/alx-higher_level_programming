3cdf5b5b886171314cfac66f42ac3a42f27e2e90
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-06-30 11:20:09 +0000

    Submit Task 0 - 5

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+rem_num = number % 10 if number > 0 else int(repr(number)[-1]) * -1
+
+print("Last digit of {} is {} ".format(number, rem_num), end="")
+
+if rem_num > 5:
+    print("and is greater than 5")
+elif rem_num == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 not 0")
