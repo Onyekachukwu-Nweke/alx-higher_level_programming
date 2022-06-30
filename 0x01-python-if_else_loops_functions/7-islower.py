eea199e007fed3c0ded61b0213e2d9094b1aa13e
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-06-30 12:42:02 +0000

    Submit 6 - 12

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return False
-    else:
+    if ord(c) > 96 and ord(c) < 123:
         return True
+    else:
+        return False
