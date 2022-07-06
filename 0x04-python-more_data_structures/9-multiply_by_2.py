685e0326a32471f3d125fb696533f758edc0c0b0
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-07-06 23:02:26 +0000

    Crime

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary.values():
-        i = i * 2
-    return a_dictionary
+    return {key: value * 2 for key, value in a_dictionary.items()}
