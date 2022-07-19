778cf5346c7f11e57314760f0d3d29ebb4fb426d
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-07-19 18:05:19 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    1-square.py: write a class Square with private instance attribute
+    size
+"""
+
+class Square:
+    """
+        Creates a class and initializes a private instance attribute
+    """
+
+
+    def __init__(self, size):
+        """
+            Initializes the instance values
+        """
+
+
+        self.__size = size
