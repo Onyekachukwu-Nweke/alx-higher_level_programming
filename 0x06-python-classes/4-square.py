41f2fd5a512bc62421a0b60de4059c9c1ad6bee4
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-07-19 19:18:30 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    Write a class that defines a Square
+
+    Initializes a value, type checks the implementation
+    and computes the Area of the square
+"""
+
+
+class Square:
+    """
+        Square Implementation
+    """
+
+    def __init__(self, size=0):
+        """
+            Initializes the attribute of the Square
+            class
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """
+            Getter for size
+        """
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            Setter for size
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+            Computes the Area of the Square
+        """
+
+        return (self.__size ** 2)
