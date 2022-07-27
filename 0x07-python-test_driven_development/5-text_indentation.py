2828febffc66110331dbad1ccf92ff972b5f25e5
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-07-27 10:58:50 +0000

    Damn

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-"""Defines a tex
+"""Defines a text-indentation function"""
+
+
+def text_indentation(text):
+    """
+    This function prints text with two new lines
+    if '.?:' is encountered
+
+    Arg(s):
+        text (str)
+
+    Raises:
+        TypeError: text must be a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for l in '.?:':
+        text = text.replace(l, "{}\n\n".format(l))
+    lines = text.splitlines()
+    for index, line in enumerate(lines):
+        print(line.strip(), end="" if index == len(lines) - 1 else "\n")
