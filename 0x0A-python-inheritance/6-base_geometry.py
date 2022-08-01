62a8850414d28e90e267908eaf24391e01262897
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-01 22:51:41 +0000

    Added Task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""This module creates a class called ``BaseGeometry`` and """
+"""
+This module creates a class called ``BaseGeometry``
+and adding of area method"""
 
 
 class BaseGeometry:
     """Representation of BaseGeometry class"""
-    def area():
+    def area(self):
         """Not implemented"""
         raise Exception("area() is not implemented")
