55b492dd2dd8a175d99e3fdb14ecfaaeff26c90c
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-01 23:43:29 +0000

    Added Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,7 +5,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
-
     def __init__(self, width, height):
         """Intialize a new Rectangle.
         Args:
@@ -16,4 +15,3 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        
