b2775138219793c96a1416995328079549eb51e6
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-01 21:39:43 +0000

    Added Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module checks if two classes are of the
+same instances"""
+
+
+def is_same_class(obj, a_class):
+    """This function checks if the two classes
+    are of the same instance
+    
+    Args:
+        obj: The object to be checked
+        a_class: Type to be checked against
+    """
+    return type(obj) == a_class
