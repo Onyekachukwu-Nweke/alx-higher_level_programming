18138e902ae9c581a9b5e1068af80b1358d5f6c9
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-02 00:25:27 +0000

    Added Task 11

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,9 @@
 class MyList(list):
     """This class inherits some properties the
     ``list`` object"""
+    def __init__(self):
+        """Initializes object"""
+        super().__init__()
+
     def print_sorted(self):
         print(sorted(self))
