e11de43956ba89c00552aad81465a683aa6e0f4b
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-02 15:58:25 +0000

    Added Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module loads files from JSON"""
+import json
+
+
+def load_from_json_file(filename):
+    """Implementation"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.loads(f)
