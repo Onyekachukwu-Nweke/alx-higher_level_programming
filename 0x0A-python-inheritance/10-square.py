9d0bca3d8b9ed12bea0f3194ebdc7541a7515eac
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-02 00:16:16 +0000

    Added Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,5 +6,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Implementation"""
     def __init__(self, size):
-        """Initialization of Square Class"""
+        """Initialization of Square Class
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
