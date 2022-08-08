5075bcb2b2bcc0906215881a620cca003d7895fa
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-08-08 21:45:04 +0000

    Added Tests for from_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,13 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        This function returns the list of JSON string
+        representation `json_string`
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
