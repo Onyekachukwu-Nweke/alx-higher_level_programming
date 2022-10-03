f061bba787eae27d7094a4ef8e821b37eec84669
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-03 11:37:29 +0000

    Task 4 - 5

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const countArgs = process.argv.length;
-console.log(countArgs === 2 ? 'No Argument' : 'Argument found');
+console.log(countArgs === 2 ? 'No Argument' : count === 3 ? 'Argument found' : 'Argument found');
