1b52a08c57ab0f63a54a04caeca120c9e0706a06
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-11 21:15:30 +0000

    [fix]: Task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,12 +9,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ Model State """
 
-
-    __tablename__ = 'states'
-
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
