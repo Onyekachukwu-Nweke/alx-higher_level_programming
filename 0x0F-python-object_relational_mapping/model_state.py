b02cf9b6d48bb84181e3857941dfa83ae6a4e9e0
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-11 21:09:32 +0000

    Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Model State """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, 
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
