29fbc014915e9e689f4316f6732aebf2a3bd828a
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-10-11 21:12:30 +0000

    [fix]: Task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,8 +12,9 @@ Base = declarative_base()
 class State(Base):
     """ Model State """
 
+
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, 
+    id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
