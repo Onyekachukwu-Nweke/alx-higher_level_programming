a5b4ace7bc34384359805644e814dd569c650e65
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-11-19 05:02:22 +0000

    Added Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    Python script that takes in a URL and an email,
+    sends a POST request to the passed URL with the email
+    as a parameter, and displays the body of the response
+    (decoded in utf-8)
+"""
+
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == "__main__":
+    value = {'email': argv[2]}
+    data = urlencode(value)
+    data = data.encode('ascii')
+    req = Request(argv[1], data)
+    with urlopen(req) as response:
+        page = response.read()
+        print(page.decode('utf-8'))
