88a9fdbda591f3277a81979c008c5d5d81aa97f5
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-11-19 04:25:20 +0000

    Task 1: Headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" 
+    Python script that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id variable found
+    in the header of the response.
+"""
+
+from urllib.request import urlopen
+from sys import argv
+
+if __name__ == "__main__":
+    with urlopen(argv[1]) as response:
+        headers = response.info()
+        Id = headers.get('X-Request-Id')
+        print(Id)
