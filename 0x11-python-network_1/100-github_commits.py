155ecdfcaf49df74d8f4a94c29e8d73b7679787c
Onyekachukwu Ejiofor Nweke <nwekeejioforscheller@gmail.com>
2022-11-19 12:24:45 +0000

    Adv Task: Github Commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    lists the 10 most recent commits on a given GitHub repository.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            argv[1], argv[2])
+
+    res = requests.get(url)
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
