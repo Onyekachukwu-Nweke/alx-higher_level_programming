7ef8527bb3eab53a19e7f8d1443251dd987e0266
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-03-20 07:49:31 +0100

    feat: task 13 @Onyekachukwu-Nweke

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Write a script that updates a State objects in
+the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(user, passwd, db), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    # To start a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State):
+        if "a" in state.name:
+            session.delete(state)
+    session.commit()
+    session.close()
